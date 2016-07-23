a671cfc098df8e13b9590db1a3b7e9d78d212fd3
HungDC <smctdndqn@yahoo.com>
2016-07-23 11:55:54 +0700

    Initial Commit

---
+++ TMDT/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TMDT
+//
+//  Created by Hung Duong on 7/23/16.
+//  Copyright © 2016 Hung Duong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
